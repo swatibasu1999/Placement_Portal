48e74829f80e185ad5730570cfb747da4106233d
Swati Basu <swati.basu1999@gmail.com>
2020-12-14 15:30:09 +0530

    navbar added

--- index.php
+++ index.php
@@ -25,30 +25,8 @@
       </head>
     <body>
       <!--NAVBAR-->
-        <section class="header">
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark header" style="height: 80px;">
-            <a class="navbar-brand" href="#"><h1>Let's Get Hired</h1></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-              <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Drive</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModal">Login/Sign-Up</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </section>
+      <?php include 'navbar.php'; ?>
+      <!--NAVBAR-->
 
         <!--CAROUSAL IMAGES WITH CAPTIONS-->
           <section class="first-side" style="height: 85%;">
@@ -92,14 +70,14 @@
           </section>
 
           <!--ABOUT SECTION- ROADMAP-->
-          <section class="about">
+          <section id="id-about" class="about">
           <div class="d-flex justify-content-center py-5">
           <img src="images/roadmap.jpeg" class="img-fluid roadmap" alt="Responsive image">
           </div>
             </section>
 
             <!--COMPANY DRIVES-->
-            <section class="company py-5">
+            <section id="company-drive" class="company py-5">
               <div class="company-header text-center py-2">
                 <h1><strong>Current Mock Drives</strong></h1>
               </div>
@@ -144,7 +122,7 @@
             </section>
 
         <!--CONTACT SECTION-->    
-        <section class="contact" >
+        <section id="id=contact" class="contact" >
           <div class="contact-heading">
             <h1>Contact Our Team</h1>
           </div>
@@ -177,6 +155,7 @@
             </div>
           </div>
         </section>
+        <!--CONTACT SECTION-->
 
        <!-- Modal LOGIN/SIGN-UP-->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
